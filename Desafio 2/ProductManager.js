f1f282f43610ab8338c2f7f708610457056c1d23
Axel Sanchez <axel.d.sanchez@gmail.com>
2023-11-28 22:53:52 -0300

    Arreglo final

--- Desafio 2/ProductManager.js
+++ Desafio 2/ProductManager.js
@@ -14,7 +14,7 @@ class ProductManager {
         }
 
         ProductManager.id++;
-         
+
         const products = await this.getProducts();
         const newProduct = {
             id: ProductManager.id,
@@ -41,12 +41,83 @@ class ProductManager {
         }
     }
 
-    async getProductsById(){
+    async getProductsById(id) {
         const products = await this.getProducts();
         const product = products.find(p => p.id === id);
-        if(!product){
+        if (!product) {
             return console.error("Producto no encontrado");
         }
         return product;
     }
+
+    async deleteProduct(id) {
+        const products = await this.getProducts();
+        const productsDeleted = products.filter(product => product.id !== id);
+        await fs.promises.writeFile(this.path, JSON.stringify(productsDeleted), 'utf-8')
+    }
+
+    async updateProduct(id, productToUpdate) {
+        const products = await this.getProducts();
+        const updateProducts = products.map(product => {
+            if (product.id === id) {
+                return {
+                    ...product,
+                    ...productToUpdate,
+                    id
+                }
+
+            }
+
+            return product;
+        });
+
+        await fs.promises.writeFile(this.path, JSON.stringify(updateProducts), 'utf-8')
+    }
 }
+
+const test = async () => {
+    const productManager = new ProductManager('./products.json');
+/*     await productManager.addProduct({
+        title: "Celular",
+        description: "S22",
+        code: 2200,
+        stock: 5,
+        thumnail: "./S22.jpg",
+        price: 150000,
+
+    });
+
+    await productManager.addProduct({
+        title: "Celular",
+        description: "S23",
+        code: 2300,
+        stock: 8,
+        thumnail: "./S23.jpg",
+        price: 180000,
+
+    });
+
+    await productManager.addProduct({
+        title: "Celular",
+        description: "S24",
+        code: 2400,
+        stock: 10,
+        thumnail: "./S24.jpg",
+        price: 200000,
+
+    }); */
+
+    const product2 = await productManager.getProductsById(2);
+    console.log(product2);
+
+    await productManager.updateProduct(1, {
+        title: "Xiaomi"
+    });
+
+    await productManager.deleteProduct(3);
+
+}
+
+
+test ();
+
